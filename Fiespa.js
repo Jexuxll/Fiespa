d6d4b49e4452eb28dfdbd6afb45d8e77b3297704
Jexuxll <jesusleallopez12@gmail.com>
2026-05-16 14:46:39 +0200

    Ajustes de tiempo flip automatico y estilos

--- Fiespa.js
+++ Fiespa.js
@@ -133,7 +133,7 @@ document.addEventListener("DOMContentLoaded", () => {
             clearTimeout(flipTimer);
             flipTimer = setTimeout(() => {
               unflipItem(item);
-            }, 1800);
+            }, 2500);
           }
         });
 
